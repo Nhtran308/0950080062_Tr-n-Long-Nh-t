0b0915e66f0a25418e9b7cbed7c9215aeb727464
Nhtran308 <118456728+Nhtran308@users.noreply.github.com>
2023-03-29 10:54:02 +0700

    Update Lab2.sql

--- Tuan2_Lab1/Lab2.sql
+++ Tuan2_Lab1/Lab2.sql
@@ -1,4 +1,4 @@
-﻿USE QLBanHang;
+USE QLBanHang;
 
 /*1
 SELECT * FROM Hangsx;
@@ -80,13 +80,48 @@ INNER JOIN Hangsx sx ON sx.Mahangsx = sp.mahangsx
 WHERE YEAR(n.ngaynhap) = 2020 AND sx.Tenhang = 'Samsung'
 */
 
-/*
+/* 12
 SELECT SUM(x.soluongX*sp.giaban) as N'Tổng tiền xuất'
 FROM Xuat x
 INNER JOIN Sanpham sp ON sp.masp = x.Masp
-WHERE DAY(x.ngayxuat) = 2 AND MONTH(x.ngayxuat) = 9 AND YEAR(x.ngayxuat) = 2018
+WHERE  x.ngayxuat = '2018-09-02'
 */
 
-SELECT sohdn, ngaynhap, tiennhap = soluongN*dongiaN
+/* 13
+SELECT TOP 1 sohdn, ngaynhap, tiennhap = soluongN*dongiaN
 FROM Nhap
-WHERE YEAR(ngaynhap) = 2020;
+WHERE YEAR(ngaynhap) = 2018
+ORDER BY tiennhap DESC
+*/
+
+/* 14
+SELECT TOP 10 *
+FROM Sanpham sp
+INNER JOIN Nhap n ON n.Masp = sp.masp
+ORDER BY n.soluongN DESC
+*/
+
+/* 15
+SELECT sp.masp, sp.tensp
+FROM Sanpham sp
+INNER JOIN Hangsx sx ON sx.Mahangsx = sp.mahangsx
+INNER JOIN Nhap n ON n.Masp = sp.masp
+INNER JOIN Nhanvien nv ON nv.Manv = n.Manv
+WHERE sx.Tenhang = 'Samsung' AND nv.Manv = 'NV01'
+*/
+
+/* 16
+SELECT n.sohdn, sp.masp, n.soluongN,  n.ngaynhap
+FROM Nhap n
+INNER JOIN Sanpham sp ON sp.masp = n.Masp
+INNER JOIN Xuat x ON x.Masp = sp.masp
+INNER JOIN Nhanvien nv ON nv.Manv = x.Manv
+WHERE sp.masp = 'SP05' AND nv.Manv = 'NV01'
+*/
+
+/* 17
+SELECT nv.manv, nv.tennv
+FROM Nhanvien nv
+INNER JOIN Xuat x ON x.Manv = nv.Manv	
+WHERE x.Masp = 'SP02' AND x.Ngayxuat = '2020-02-03'
+*/
